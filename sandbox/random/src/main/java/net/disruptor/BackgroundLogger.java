8f0a16f3f968cca550c1e6ec5b0afa83b57202c6
Anatoly Lupacescu <alupacescu@vivatinc.com>
2013-03-28 12:14:26 +0200

    fixed disruptor

--- sandbox/random/src/main/java/net/disruptor/BackgroundLogger.java
+++ sandbox/random/src/main/java/net/disruptor/BackgroundLogger.java
@@ -1,45 +1,36 @@
 package net.disruptor;
 
-import com.lmax.disruptor.RingBuffer;
-import com.lmax.disruptor.dsl.Disruptor;
-
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class BackgroundLogger
-{
-    private static final int ENTRIES = 64;
+import com.lmax.disruptor.RingBuffer;
+import com.lmax.disruptor.dsl.Disruptor;
 
-    private final ExecutorService executorService;
-    private final Disruptor<LogEntry> disruptor;
-    private final RingBuffer<LogEntry> ringBuffer;
+public class BackgroundLogger {
 
-    public BackgroundLogger()
-    {
-        executorService = Executors.newCachedThreadPool();
-        disruptor = new Disruptor<LogEntry>(LogEntry.FACTORY, ENTRIES, executorService);
-        disruptor.handleEventsWith(new LogEntryHandler());
-        disruptor.start();
-        ringBuffer = disruptor.getRingBuffer();
-    }
+	private final ExecutorService executorService;
+	private final Disruptor<LogEntry> disruptor;
+	private final RingBuffer<LogEntry> ringBuffer;
 
-    public void log(String text)
-    {
-        final long sequence = ringBuffer.next();
-        final LogEntry logEntry = ringBuffer.getPublished(sequence);
+	@SuppressWarnings("unchecked")
+	public BackgroundLogger() {
+		executorService = Executors.newCachedThreadPool();
+		disruptor = new Disruptor<LogEntry>(LogEntry.FACTORY, 64, executorService);
+		disruptor.handleEventsWith(new LogEntryHandler()); 
+		ringBuffer = disruptor.start();
+	}
 
-        logEntry.time = System.currentTimeMillis();
-        logEntry.level = 1;
-        logEntry.text = text;
+	public void log(String text) {
+		final long sequence = ringBuffer.next();
+		final LogEntry logEntry = ringBuffer.getPreallocated(sequence);
 
-        ringBuffer.publish(sequence);
-    }
+		logEntry.setText(text);
 
-    public void stop()
-    {
-        disruptor.shutdown();
-        executorService.shutdownNow();
-    }
+		ringBuffer.publish(sequence);
+	}
+
+	public void stop() {
+		disruptor.shutdown();
+		executorService.shutdownNow();
+	}
 }
-
-
