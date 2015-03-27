52428ba64e4c7f924efc4d66e374c82ab636bb06
Anatolie Lupacescu <Anatolie.Lupacescu@netent.com>
2015-03-27 12:20:25 +0100

    removed petclinic

--- petclinic/src/main/java/org/springframework/samples/petclinic/repository/ExceptionRepository.java
+++
@@ -1,9 +0,0 @@
-package org.springframework.samples.petclinic.repository;
-
-import org.springframework.dao.DataAccessException;
-import org.springframework.samples.petclinic.model.ExceptionLog;
-
-public interface ExceptionRepository {
-
-	void log(ExceptionLog ex) throws DataAccessException;
-}
